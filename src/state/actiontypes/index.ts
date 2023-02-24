1cc098ade5eca767b089458f4ad37908eb689cb2
Leta Keane <leta@hey.com>
2023-02-23 20:58:18 -0700

    WIP through Udemy tutorial

---
+++ src/state/actiontypes/index.ts
@@ -0,0 +1,5 @@
+export enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repostories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repostories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repostories_error',
+}
